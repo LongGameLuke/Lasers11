14fa110ff0b10c72bea71cf093353e44745746b4
DaBlaze <7017951+DaBlaze@users.noreply.github.com>
2026-01-22 18:44:28 -0600

    Added boiler plate code and requirements file

---
+++ run.py
@@ -0,0 +1,22 @@
+def start_message():
+    print("""    ____  __          __            
+   ╱ __ ╲╱ ╱_  ____  ╱ ╱_____  ____ 
+  ╱ ╱_╱ ╱ __ ╲╱ __ ╲╱ __╱ __ ╲╱ __ ╲
+ ╱ ____╱ ╱ ╱ ╱ ╱_╱ ╱ ╱_╱ ╱_╱ ╱ ╱ ╱ ╱
+╱_╱   ╱_╱ ╱_╱╲____╱╲__╱╲____╱_╱ ╱_╱ 
+                                    """)
+    print("Welcome to Photon Laser Tag.")
+    print("========================================")
+    print("\tCreated by Team 11:")
+    print("\t\tDayo Arigbede")
+    print("\t\tBrendyn Burgess")
+    print("\t\tBrad Chailland")
+    print("\t\tAngel Duron")
+    print("\t\tLuke Fletcher")
+    print("========================================\n")
+
+if __name__ == "__main__":
+    start_message()
+    print("Press ENTER to close program.", end="")
+    input()
+    exit(1)
