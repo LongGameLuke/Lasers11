2ddcc6d9bcb6b06ce260639e0e82f5ea6a19d00d
DaBlaze <7017951+DaBlaze@users.noreply.github.com>
2026-02-03 14:37:30 -0600

    New light blue ascii color and process log type

--- modules/consolelog.py
+++ modules/consolelog.py
@@ -6,13 +6,14 @@ class Color(Enum):
     GREEN = "\033[32m"
     YELLOW = "\033[33m"
     BLUE = "\033[34m"
+    LIGHT_BLUE = "\033[96m"
     MAGENTA = "\033[35m"
     CYAN = "\033[36m"
     WHITE = "\033[37m"
     RESET = "\033[0m"
 
 def log_process_start(message):
-    print(f"[{Color.MAGENTA.value}~{Color.RESET.value}] {message}...")
+    print(f"[{Color.MAGENTA.value}-{Color.RESET.value}] {message}...")
 
 def log_process_error(message, error=None):
     print(f"[{Color.RED.value}!{Color.RESET.value}] {Color.RED.value}ERROR{Color.RESET.value}: {message}")
@@ -21,3 +22,6 @@ def log_process_error(message, error=None):
 
 def log_process_complete(message):
     print(f"[{Color.GREEN.value}+{Color.RESET.value}] {message}!")
+
+def log_process(message):
+    print(f"[{Color.LIGHT_BLUE.value}***{Color.RESET.value}] {message}")
