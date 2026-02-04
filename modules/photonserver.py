6a548c3386a8e70f181148ca9e584fd14ac9d563
DaBlaze <7017951+DaBlaze@users.noreply.github.com>
2026-02-04 15:49:09 -0600

    Server can now interact with basic traffic generator

--- modules/photonserver.py
+++ modules/photonserver.py
@@ -1,4 +1,12 @@
 import socket
+from enum import Enum
+
+class SERVER_CODES(Enum):
+    START = "202"
+    END = "221"
+    RED_BASE_HIT = "53"
+    GREEN_BASE_HIT = "43"
+
 
 class PhotonServer:
     def __init__(self, host:str, ports:dict):
@@ -7,8 +15,10 @@ class PhotonServer:
         self.broadcast_port = ports["broadcast"]
         self.receive_port = ports["receive"]
         self.bufferSize = 1024
-        self.msgFromServer = "Hello UDP Client"
-        self.bytesToSend = str.encode(self.msgFromServer)
+
+        # Game status vars
+        self.game_in_progress:bool = False
+        self.players_tagged = 0
 
         # Datagram socket
         self.udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -16,13 +26,23 @@ class PhotonServer:
         # Bind to address and ip
         self.udp_server_socket.bind((self.host, self.receive_port))
 
-    def send_message(self, reply_address:str) -> None:
+    def broadcast_message(self, message:str) -> None:
         # Sends a reply to client address
-        self.udp_server_socket.sendto(self.bytesToSend, reply_address)
+        encoded_message = str.encode(message)
+        self.udp_server_socket.sendto(encoded_message, (self.host, self.broadcast_port))
     
     def start_game(self) -> None:
-        start_code = str.encode("202")
+        # Send the start game code to clients
+        start_code = str.encode(SERVER_CODES.START.value)
+        self.game_in_progress = True
         self.udp_server_socket.sendto(start_code, (self.host, self.broadcast_port))
+
+    def end_game(self) -> None:
+        # Send the end game code to clients 3 times
+        end_code = str.encode(SERVER_CODES.END.value)
+        for i in range(3):
+            self.udp_server_socket.sendto(end_code, (self.host, self.broadcast_port))
+        self.game_in_progress = False
 
     def set_ports(self, broadcast:int, receive:int) -> None:
         # Sets current ports to new ones in event user changes them
@@ -34,8 +54,15 @@ class PhotonServer:
         bytesAddressPair = self.udp_server_socket.recvfrom(self.bufferSize)
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP  = "Client IP Address:{}".format(address)
+        clientMsg = f"Message from Client:{message}"
+        clientIP  = f"Client IP Address:{address}"
 
         print(f"{clientIP}: {clientMsg}")
-        self.send_message(address)
+
+        self.players_tagged += 1
+        clientMsg = clientMsg.strip("'")
+        hit_equipment = clientMsg.split(":")
+        self.broadcast_message(hit_equipment[2])
+
+        if self.players_tagged >= 6:
+            self.end_game()
