415825c3d6e003e4e841d199eaf717cca9cec1fa
DaBlaze <7017951+DaBlaze@users.noreply.github.com>
2026-03-03 10:10:04 -0600

    Various changes to MusicPlayer

--- modules/ui/musicplayer.py
+++ modules/ui/musicplayer.py
@@ -2,13 +2,11 @@ import pygame
 import random
 
 class MusicPlayer:
-    def __init__(self, track_list):
+    def __init__(self, track_list:list):
         pygame.mixer.init()
-        self.track_list = track_list
+        self.track_list:list = track_list
         self.loaded_song = None
-
-    def load_track(self, track_index:int):
-        pass
+        self.volume:float = 1.0
     
     def load_track_random(self):
         # Loads random audio track from track_list into loaded_song
@@ -20,20 +18,25 @@ class MusicPlayer:
     def play(self):
         # Plays currently loaded audio file
 
-        # Return if no song has been loaded
+        # Checks to make sure audio is not playing
+        # and audio is loaded
         if self.loaded_song == None:
             print("No song to play. No song loaded.")
+            return
+        elif self.is_playing():
+            print("Song is already playing.")
             return
         
         pygame.music.mixer.play()
 
     def stop(self):
         # Stops currently playing audio
-        if self.isPlaying == False:
+        if self.is_playing() == False:
             print("No music is currently playing")
             return
         
         pygame.music.mixer.stop()
 
     def is_playing(self)->bool:
+        # Returns if music is currently playing
         return pygame.music.mixer.get_busy()
